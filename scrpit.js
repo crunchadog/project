7077fcaf9b965ba0596adc5ba0bb8d03d9927c83
Kirill <bobryshov@niuitmo.ru>
2022-11-29 18:22:30 +0300

    push1

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+const age = () => {
+    if (age => 18) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = age;
---
+++ scrpit_test.js
@@ -0,0 +1,5 @@
+const age = require('./scrpit');
+
+test('1 + 1', () => {
+    expect(age(50)).toBe(true);
+})
